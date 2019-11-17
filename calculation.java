3bdc01d71cb1939b744155bcf2bf04e16ebd05ae
Kamal-prog-code <52599247+Kamal-prog-code@users.noreply.github.com>
2019-11-17 20:17:42 +0530

    Update calculation.java

--- calculation.java
+++ calculation.java
@@ -12,14 +12,18 @@ public class calculation
 		add a = new add();
 		div d = new div();
 		sub s = new sub();
+		mul m = new mul();
 		int ad = a.Addition(x,y);
 		int su = s.Sub(x,y);
 		int di = d.Div(x,y);
-		System.out.println("Pick up the choices 1.add 2.sub 3.div");
+		int mu = m.Mul(x,y);
+		System.out.println("Pick up the choices 1.add 2.sub 3.mul 4.div");
 		if(choice=="1")
 		{System.out.println("Addition : "+ad);}
 		else if(choice=="2")
 		{System.out.println("Subtraction : "+su);}
+		else if(choice=="3")
+		{System.out.println("Multiplication : "+mu);}
 		else
 		{System.out.println("Division : "+di);}
 	}
